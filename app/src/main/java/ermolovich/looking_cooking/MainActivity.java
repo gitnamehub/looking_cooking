1c39558c37429cde0bab0b694c7d450dbc25bd88
Alexandr Yermolovich <developalex@gmail.com>
2016-11-29 23:15:40 +0200

    Initial commit

---
+++ app/src/main/java/ermolovich/looking_cooking/MainActivity.java
@@ -0,0 +1,13 @@
+package ermolovich.looking_cooking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
